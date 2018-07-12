2b38db4090461c1d3795d58e21dfe61aad0bd53a
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-07-12 14:15:26 -0700

    implement datepicker

--- src/public/datepicker.js
+++ src/public/datepicker.js
@@ -1,6 +1,5 @@
 if (document.getElementById('ud-component-daterangepicker-template')) {
   Vue.component('press-daterangepicker', {
-    inheritAttrs: false,
     props: {
       value: {
         default: function() {
@@ -27,12 +26,31 @@ if (document.getElementById('ud-component-daterangepicker-template')) {
         end: null
       };
     },
+    computed: {
+      startDate() {
+        if (!this.start) {
+          return null;
+        }
+
+        return this.start.toISOString().replace(/T.+$/, '');
+      },
+      endDate() {
+        if (!this.end) {
+          return null;
+        }
+
+        return this.end.toISOString().replace(/T.+$/, '');
+      }
+    },
     methods: {
       emit: function() {
         this.$emit('input', {
-          start: this.start,
-          end: this.end
+          start: this.startDate,
+          end: this.endDate
         });
+      },
+      onFocus: function() {
+        this.refs.datepicker.showDatepicker();
       },
       onCheckinChanged: function(newDate) {
         this.start = newDate;

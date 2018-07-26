3225cf765d3f79bb71b215c751caa51ebcf15803
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-07-26 11:36:18 -0700

    refactor(press): make press.js a class and execute lifecycle within

--- src/internal.d.ts
+++ src/internal.d.ts
@@ -6,24 +6,22 @@ declare interface HTMLElement {
   after<T extends Node>(...nodes: T[]): void;
 }
 
-declare namespace Press {
-  interface PressComponent {
-    name: string;
-    infer?: Inferer;
-    enhance: Enhancer;
-  }
-
-  /**
-   * An Enhancer receives a node identified by `data-press-component` and
-   * adds additional attributes, children, or siblings to it. Typically, this
-   * means adding Vue directives.
-   */
-  type Enhancer = (el: Element | HTMLElement) => void;
-
-  /**
-   * An Inferer receives a node (typically `document`) and searches its children
-   * for nodes of particular criteria, then adds the appropriate `data-press`
-   * attributes to those nodes.
-   */
-  type Inferer = (el: Document | Element | HTMLElement) => void;
+interface PressComponent {
+  name: string;
+  infer?: PressComponentInferrer;
+  enhance: PressComponentEnhancer;
 }
+
+/**
+ * An Enhancer receives a node identified by `data-press-component` and
+ * adds additional attributes, children, or siblings to it. Typically, this
+ * means adding Vue directives.
+ */
+type PressComponentEnhancer = (el: Element | HTMLElement) => void;
+
+/**
+ * An Inferer receives a node (typically `document`) and searches its children
+ * for nodes of particular criteria, then adds the appropriate `data-press`
+ * attributes to those nodes.
+ */
+type PressComponentInferrer = (el: Document | Element | HTMLElement) => void;
--- src/press.js
+++ src/press.js
@@ -1,37 +1,147 @@
 import {logger} from './lib/logger';
+import {vueify} from './lib/vueify';
 
-/** @type {Map<string, Press.PressComponent>} */
-const components = new Map();
-
-/**
- * @param {Element|HTMLElement} el
- */
-export function enhanceComponent(el) {
-  if (!(el instanceof HTMLElement)) {
-    return;
+export class Press {
+  get components() {
+    return components.get(this);
   }
-  logger.info('Enhancing component');
-  const componentName = el.dataset.pressComponent;
-  logger.info(`Finding enhancer for ${componentName}`);
-  const component = components.get(componentName);
-  if (!component) {
-    logger.warn(
-      `Component "${componentName}" has not be registered with PRESS`
+
+  get logger() {
+    return loggers.get(this);
+  }
+
+  /**
+   * @param {{logger: Console}} options
+   */
+  constructor({logger = console} = {logger: console}) {
+    components.set(this, new Map());
+    loggers.set(this, logger);
+  }
+
+  /**
+   * @param {PressComponent} component
+   */
+  registerComponent(component) {
+    logger.info(`Registering component ${component.name}`);
+    this.components.set(component.name, component);
+    logger.info(`Registered component ${component.name}`);
+  }
+
+  /**
+   * Invokes each registered component's {@link PressComponent#infer()} method.
+   * @private
+   */
+  infer() {
+    performance.mark('press:infer:components:start');
+    logger.info('Inferring component types');
+
+    for (const [name, component] of this.components) {
+      performance.mark(`press:infer:components:${name}:start`);
+      logger.info(`Inferring ${name}`);
+
+      component.infer(document);
+
+      logger.info(`Inferred ${name}`);
+      performance.mark(`press:infer:components:${name}:end`);
+    }
+
+    logger.info('Inferred component types');
+    performance.mark('press:infer:components:end');
+  }
+
+  /**
+   * Invokes the appropriate component's {@link PressComponent#enhance()} method
+   * for each element matching `[data-press-component]`.
+   * @private
+   */
+  enhance() {
+    performance.mark('press:enhance:components:start');
+    logger.info('Enhancing components');
+
+    Array.from(document.querySelectorAll('[data-press-component]')).forEach(
+      (el) => this.enhanceComponent(el)
     );
-    return;
+
+    logger.info('Enhanced components');
+    performance.mark('press:enhance:components:end');
   }
-  logger.info(`Found enhancer for ${componentName}`);
 
-  logger.info(`Enhancing element with ${componentName}`);
-  component.enhance(el);
-  logger.info(`Enhanced element with ${componentName}`);
+  /**
+   * Enhances the specified element
+   * @param {Element|HTMLElement} el
+   * @private
+   */
+  enhanceComponent(el) {
+    if (!(el instanceof HTMLElement)) {
+      return;
+    }
+    logger.info('Enhancing component');
+    const componentName = el.dataset.pressComponent;
+    logger.info(`Finding enhancer for ${componentName}`);
+    const component = this.components.get(componentName);
+    if (!component) {
+      logger.warn(
+        `Component "${componentName}" has not be registered with PRESS`
+      );
+      return;
+    }
+    logger.info(`Found enhancer for ${componentName}`);
+
+    logger.info(`Enhancing element with ${componentName}`);
+    component.enhance(el);
+    logger.info(`Enhanced element with ${componentName}`);
+  }
+
+  /**
+   * Activates a Vue instance for each discovered `[data-press-app]` element
+   * @private
+   */
+  activate() {
+    performance.mark('press:activate:start');
+    logger.info('Vueifying non-apped PRESS component');
+    Array.from(document.querySelectorAll('[data-press-app]')).forEach(
+      (root) => {
+        if (!(root instanceof HTMLElement)) {
+          return;
+        }
+        vueify(root);
+      }
+    );
+    logger.info('Vueified non-apped PRESS component');
+    performance.mark('press:activate:end');
+  }
+
+  /**
+   * Generates a report from the marked performance entries and logs it to the
+   * console, prefferring the table() method if available.
+   */
+  report() {
+    const results = performance
+      .getEntriesByType('mark')
+      .filter(({name}) => name.startsWith('press'));
+
+    if (logger.table) {
+      logger.table(results.map(({name, startTime}) => [name, startTime]));
+    } else {
+      logger.info(results.map(({name, startTime}) => ({name, startTime})));
+    }
+  }
+
+  /**
+   * Executes PRESS lifecycle methods
+   */
+  run() {
+    this.infer();
+    this.enhance();
+    this.activate();
+    if (process.env.NODE_ENV !== 'production') {
+      this.report();
+    }
+  }
 }
 
-/**
- * @param {Press.PressComponent} component
- */
-export function registerComponent(component) {
-  logger.info(`Registering component ${component.name}`);
-  components.set(component.name, component);
-  logger.info(`Registered component ${component.name}`);
-}
+/** @type {WeakMap<Press, Map<string, PressComponent>>} */
+const components = new WeakMap();
+
+/** @type {WeakMap<Press, Console>} */
+const loggers = new WeakMap();

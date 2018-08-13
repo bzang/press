53b91f4f20297a7e7d96a5cbfb1c643b2b71402d
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-08-13 17:26:30 -0500

    ci(wdio): use spec reporter in CI

--- wdio.conf.js
+++ wdio.conf.js
@@ -198,7 +198,7 @@ exports.config = {
   // Test reporter for stdout.
   // The only one supported by default is 'dot'
   // see also: http://webdriver.io/guide/reporters/dot.html
-  reporters: [CI && 'dot', !CI && 'spec', 'junit'].filter(Boolean),
+  reporters: [CI && 'dot', 'spec', 'junit'].filter(Boolean),
   //
   // If you are using Cucumber you need to specify the location of your step definitions.
   cucumberOpts: {

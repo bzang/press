1e7455c4b30e7f7eb1f1e9011a56f3239015ca72
Ian W. Remmel <1182361+ianwremmel@users.noreply.github.com>
2018-08-16 14:20:15 -0500

    feat(core): only call press.run() in CDN builds

--- src/index.js
+++ src/index.js
@@ -4,11 +4,19 @@ import * as datepicker from './components/datepicker';
 import * as daterangepicker from './components/daterangepicker';
 import {logger} from './lib/logger';
 
-const press = new Press({logger});
+export const press = new Press({logger});
+
 performance.mark('press:register:components:start');
 press.registerComponent(autocomplete);
 press.registerComponent(datepicker);
 press.registerComponent(daterangepicker);
 performance.mark('press:register:components:end');
 
-press.run();
+if (process.env.BUILD_TARGET === 'cdn' || process.env.AUTORUN) {
+  press.run();
+}
+
+if (process.env.BUILD_TARGET === 'cdn') {
+  //@ts-ignore
+  window.press = press;
+}
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {EnvironmentPlugin} = require('webpack');
 
 const PROD = process.env.NODE_ENV === 'production';
 const CDN = process.env.BUILD_TARGET === 'cdn';
@@ -51,6 +52,10 @@ module.exports = {
   // paths:true is needed for get/has/set deep path support which is pretty much
   // the whole reason we're loading lodash in the first place
   plugins: [
+    new EnvironmentPlugin({
+      AUTORUN: false,
+      BUILD_TARGET: ''
+    }),
     new LodashModuleReplacementPlugin({paths: true}),
     PROD &&
       new MiniCssExtractPlugin({
